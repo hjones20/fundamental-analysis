bab418e10c7b35f51d34b92cceed4e261eb6ab51
hjones20 <chulahoma1@gmail.com>
2020-04-20 12:29:47 -0400

    Create company_information.py file

---
+++ company_information.py
@@ -0,0 +1,16 @@
+'''
+Fetch the following company attributes from the Financial Modeling Prep API and store them in a CSV
+
+- symbol
+- company Name
+- exchange
+- industry
+- website
+- description
+- ceo
+- sector
+
+source: https://financialmodelingprep.com/developer/docs/#Company-Profile
+'''
+
+print('hello')
