c31a3c370fa2ed27935b5838970a31548232e524
hjones20 <chulahoma1@gmail.com>
2020-04-21 13:07:59 -0400

    Alter select industry, sector functions to accept df as an argument

--- fundamental_analysis.py
+++ fundamental_analysis.py
@@ -1,5 +1,6 @@
 import FundamentalAnalysis as fa
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Set global printing options
 pd.options.display.max_columns = 20
@@ -7,19 +8,23 @@ pd.options.display.max_rows = 100
 
 # Fetch the available company profiles
 company_profiles = pd.read_csv('company_profiles.csv')
-print(company_profiles.head())
+
+# Select your circle of competence
+sector_company_counts = company_profiles['sector'].value_counts(dropna=False)
+industry_company_counts = company_profiles['industry'].value_counts(dropna=False)
 
 
-def select_industry(industry):
-    industry_filter = company_profiles['industry'] == industry
-    industry_profiles = company_profiles[industry_filter]
+def select_sector(df, sector):
+    sector_filter = df['sector'] == sector
+    sector_profiles = df[sector_filter]
+    return sector_profiles
+
+
+def select_industry(df, industry):
+    industry_filter = df['industry'] == industry
+    industry_profiles = df[industry_filter]
     return industry_profiles
 
-
-def select_sector(sector):
-    sector_filter = company_profiles['sector'] == sector
-    sector_profiles = company_profiles[sector_filter]
-    return sector_profiles
 
 
 
