478a3cdb16d227b1b7da571472e7be3990c65e41
hjones20 <chulahoma1@gmail.com>
2020-04-20 23:04:14 -0400

    Filter outstock tickers  without a company name from ticker_company_mapping.csv

--- financial_modeling_api.py
+++ financial_modeling_api.py
@@ -2,6 +2,10 @@ from urllib.request import urlopen
 import json
 import pandas as pd
 
+# Set global printing options
+pd.options.display.max_columns = 20
+pd.options.display.max_rows = 100
+
 
 def fetch_companies():
     """
@@ -20,11 +24,35 @@ def fetch_companies():
             # All elements of <list> data_json have a symbol (ticker), but not necessarily a company name
             print('Company name does not exist for stock ticker: ' + str(company['symbol']))
 
-    available_companies = pd.DataFrame(ticker_company_map, index=["symbol"]).T.sort_index()
-    available_companies.to_csv('ticker_company_mapping.csv')
+    available_listings = pd.DataFrame(ticker_company_map, index=["symbol"]).T.sort_index()
+    complete_listings = available_listings.drop('')
+    complete_listings.to_csv('ticker_company_mapping.csv')
 
-    return available_companies
+    return complete_listings
+
+fetch_companies()
 
 
-
-
+#
+# #ticker_csv = pd.read_csv('ticker_company_mapping.csv', index_col=0)
+# print(ticker_csv)
+#
+# company_data = []
+#
+# for ticker in ticker_csv['symbol'][0:3]:
+#     response = urlopen("https://financialmodelingprep.com/api/v3/company/profile/" + ticker)
+#     data = response.read().decode("utf-8")
+#     data_json = json.loads(data)['profile']
+#     company_data.append(data_json)
+#
+# company_df = pd.DataFrame(company_data)
+# company_df = company_df[['companyName', 'exchange', 'industry', 'website', 'description', 'ceo', 'sector']]
+#
+# # Remove rows with missing values
+# company_df = company_df.loc[company_df['companyName'] != '']
+#
+# # Merge with ticker_csv to capture ticker; join on companyName
+#
+#
+# # print(company_profile)
+# # print(len(company_profile))
