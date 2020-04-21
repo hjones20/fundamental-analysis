707a5fcc62a06b5af95b672490cc065cd8c1cbeb
hjones20 <chulahoma1@gmail.com>
2020-04-21 12:23:26 -0400

    Create select industry and select sector functions

--- fundamental_analysis.py
+++ fundamental_analysis.py
@@ -1,50 +1,25 @@
 import FundamentalAnalysis as fa
+import pandas as pd
 
-# Fetch the available companies
-companies = fa.available_companies()
+# Set global printing options
+pd.options.display.max_columns = 20
+pd.options.display.max_rows = 100
 
-# Pull row names (tickers) from pandas DF returned above and cast to a list
-tickers = list(companies.index)
-
-# Initialize empty dict to store company-sector mappings
-company_sector_map = {}
-
-
-# Create function to perform company-sector mapping
-def sector_mapping():
-    for ticker in tickers[0:500]:
-        company_profile = fa.profile(ticker)
-        company_sector_map[ticker] = company_profile.loc['sector', 'profile']
-    return company_sector_map
+# Fetch the available company profiles
+company_profiles = pd.read_csv('company_profiles.csv')
+print(company_profiles.head())
 
 
-sector_mapping()
+def select_industry(industry):
+    industry_filter = company_profiles['industry'] == industry
+    industry_profiles = company_profiles[industry_filter]
+    return industry_profiles
 
 
-# Initialize empty list to store tickers of a specific sector
-sector_tickers = []
-
-
-# Create function to subset company_sector_map to a specific sector
-def sector_selection(sector):
-    for key, value in company_sector_map.items():
-        if value == sector:
-            sector_tickers.append(key)
-    return sector_tickers
-
-
-sector_selection('Technology')
-print(sector_tickers)
+def select_sector(sector):
+    sector_filter = company_profiles['sector'] == sector
+    sector_profiles = company_profiles[sector_filter]
+    return sector_profiles
 
 
 
-# ticker = 'AAPL'
-# print(fa.profile('AAPL'))
-# print(fa.rating('AAPL'))
-# print(fa.income_statement(ticker, period="annual"))
-# print(fa.balance_sheet_statement(ticker, period="annual"))
-# print(fa.discounted_cash_flow(ticker, period="annual"))
-# print(fa.key_metrics(ticker, period="annual"))
-# print(fa.financial_ratios(ticker))
-# print(fa.financial_statement_growth(ticker, period="annual"))
-# print(fa.profile(ticker))
