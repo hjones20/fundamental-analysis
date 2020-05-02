a76753d5501b01a02bfa785fb38f9a7d6000e0ef
hjones20 <chulahoma1@gmail.com>
2020-05-01 20:53:28 -0400

    Refactor create_company_profile function for speed

--- fundamental/financial_modeling_api.py
+++ fundamental/financial_modeling_api.py
@@ -20,7 +20,7 @@ def get_company_data():
     data_json = json.loads(data)['symbolsList']
     flattened_data = pd.json_normalize(data_json)
 
-    print('Found stock data on ' + str(flattened_data.symbol.nunique()) + ' companies!')
+    print('Found stock data on ' + str(flattened_data.symbol.nunique()) + ' companies! \n')
 
     return flattened_data
 
@@ -37,7 +37,7 @@ def clean_company_data(df):
 
     df = df.dropna()
 
-    print('Found ' + str(df.symbol.nunique()) + ' companies with clean stock data!')
+    print('Found ' + str(df.symbol.nunique()) + ' companies with clean stock data! \n')
 
     return df
 
@@ -50,7 +50,7 @@ def select_stock_exchanges(df):
     :return: A subset of the DataFrame provided
     :rtype: pandas.DataFrame
     """
-    print('Searching for stocks listed on major exchanges among ' + str(df.symbol.nunique()) +
+    print('Searching for stocks listed on major stock exchanges among ' + str(df.symbol.nunique()) +
           ' companies...')
 
     major_stock_exchanges = ['Nasdaq Global Select', 'NasdaqGS', 'Nasdaq',
@@ -58,7 +58,7 @@ def select_stock_exchanges(df):
 
     df = df[df['exchange'].isin(major_stock_exchanges)]
 
-    print('Found ' + str(df.symbol.nunique()) + ' companies listed on major stock exchanges!')
+    print('Found ' + str(df.symbol.nunique()) + ' companies listed on major stock exchanges! \n')
 
     return df
 
@@ -77,42 +77,43 @@ def select_minimum_price(df, min_price=5.00):
 
     df = df[df['price'] >= min_price]
 
-    print('Found ' + str(df.symbol.nunique()) + ' companies that meet your price requirement!')
+    print('Found ' + str(df.symbol.nunique()) + ' companies that meet your price requirement! \n')
 
     return df
+
+
+def create_company_profile(df):
+
+    print('Searching for profile data on ' + str(df.symbol.nunique()) + ' companies...')
+
+    profile_data = pd.DataFrame()
+
+    for ticker in df['symbol']:
+        response = urlopen("https://financialmodelingprep.com/api/v3/company/profile/" + ticker)
+        data = response.read().decode("utf-8")
+        data_json = json.loads(data)['profile']
+
+        try:
+            flattened_data = pd.json_normalize(data_json)
+            flattened_data.insert(0, 'symbol', ticker)
+            profile_data = pd.concat([profile_data, flattened_data], ignore_index=True)
+
+        except KeyError:
+            continue
+
+    company_profile = profile_data[['symbol', 'companyName', 'sector', 'industry', 'exchange',
+                                    'ceo', 'description', 'website', 'mktCap', 'volAvg', 'beta',
+                                    'price']]
+
+    print('Found ' + str(company_profile.symbol.nunique()) + ' company profiles! \n')
+
+    return company_profile
 
 
 fmp_data = get_company_data()
 clean_data = clean_company_data(fmp_data)
 select_data = select_stock_exchanges(clean_data)
 price_data = select_minimum_price(select_data)
-print(price_data.head())
+test_dat = price_data[:5]
 
-# def create_company_profile(df):
-#     """
-#     Construct a company profile given a set of stock tickers.
-#
-#     Creates necessary association between stock tickers and company attributes.
-#
-#     :return: A DataFrame object containing select company attributes, joined with provided tickers
-#     :rtype: pandas.DataFrame
-#     """
-#     stock_tickers = pd.read_csv('ticker_company_mapping.csv', index_col=0)
-#
-#     company_data = []
-#
-#     for ticker in stock_tickers['symbol']:
-#         response = urlopen("https://financialmodelingprep.com/api/v3/company/profile/" + ticker)
-#         data = response.read().decode("utf-8")
-#         data_json = json.loads(data)['profile']
-#         company_data.append(data_json)
-#
-#     company_df = pd.DataFrame(company_data)
-#     company_df = company_df[['companyName', 'sector', 'industry', 'exchange', 'ceo', 'description',
-#                              'website', 'mktCap', 'volAvg', 'beta', 'price']]
-#     # company_df = company_df.set_index('companyName')
-#
-#     company_profile = company_df.merge(stock_tickers, right_index=True, left_index=True,
-#                                        how='inner')
-#
-#     return company_profile
+
