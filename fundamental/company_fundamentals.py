03102267a1ca7ff07b92f6f7d3a8fd73aa583a8f
hjones20 <chulahoma1@gmail.com>
2020-05-03 13:50:12 -0400

    Remove select_sector and select_industry functions, add logic back to company_financials.py file

--- fundamental/company_fundamentals.py
+++ fundamental/company_fundamentals.py
@@ -4,39 +4,6 @@ pd.options.display.max_columns = 20
 pd.options.display.max_rows = 100
 
 
-def select_sector(df, sector):
-    """
-    :param df: takes a df containing a 'sector' column
-    :param sector: takes a string representing a sector
-    :return: a df containing only companies in the specified sector
-    """
-    print('Evaluating sector membership among ' + str(df['symbol'].nunique()) + ' companies...')
-
-    sector_filter = df['sector'] == sector
-    df = df[sector_filter]
-
-    print('Found ' + str(df['symbol'].nunique()) + ' companies in the ' + sector + ' sector!')
-
-    return df
-
-
-# TODO: industry param needs to accept list of industry names
-def select_industry(df, industry):
-    """
-    :param df: takes a df containing a 'industry' column
-    :param industry: takes a string representing an industry
-    :return: a df containing only companies in the specified industry
-    """
-    print('Evaluating industry membership among ' + str(df['symbol'].nunique()) + ' companies...')
-
-    industry_filter = df['industry'] == industry
-    industry_profiles = df[industry_filter]
-
-    print('Found ' + str(len(industry_profiles)) + ' companies in the ' + industry + ' industry!')
-
-    return industry_profiles
-
-
 # profitability
 def calculate_profitability_metrics(df):
     pass
