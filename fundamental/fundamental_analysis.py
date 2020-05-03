ea92c2a37a4fbfa7c847db4146babe2a15e67137
hjones20 <chulahoma1@gmail.com>
2020-05-02 21:22:02 -0400

    Update docstrings, add main function

--- fundamental/fundamental_analysis.py
+++ fundamental/fundamental_analysis.py
@@ -6,74 +6,49 @@ pd.options.display.max_columns = 20
 pd.options.display.max_rows = 100
 
 
-def select_sector(df, sector):
-    """
-    :param df: takes a df containing a 'sector' column
-    :param sector: takes a string representing a sector
-    :return: a df containing only companies in the specified sector
-    """
-    print('Evaluating sector membership among ' + str(df['symbol'].nunique()) + ' companies...')
-
-    sector_filter = df['sector'] == sector
-    df = df[sector_filter]
-
-    print('Found ' + str(len(sector_profiles)) + ' companies in the ' + sector + ' sector!')
-
-    return df
-
-
-# TODO: industry param needs to accept list of industry names
-def select_industry(df, industry):
-    """
-    :param df: takes a df containing a 'industry' column
-    :param industry: takes a string representing an industry
-    :return: a df containing only companies in the specified industry
-    """
-    print('Evaluating industry membership among ' + str(df['symbol'].nunique()) + ' companies...')
-
-    industry_filter = df['industry'] == industry
-    industry_profiles = df[industry_filter]
-
-    print('Found ' + str(len(industry_profiles)) + ' companies in the ' + industry + ' industry!')
-
-    return industry_profiles
-
-
 def get_financial_statement(df, statement, period):
     """
-    :param df: takes a df with a symbol (ticker) column
-    :param statement: requires one of: income-statement, balance-sheet, or cash-flow-statement
-    :param period: requires on of: annual, quarterly
-    :return: a df containing all financial statement data
+    Retrieve financial statement data for all stock tickers in the provided DataFrame
+
+    :param df: DataFrame containing a 'symbol' column
+    :param statement: String 'income-statement', 'balance-sheet-statement', or 'cash-flow-statement'
+    :param period: String 'annual' or 'quarter'
+    :return: DataFrame containing chosen financial statement data for all years available
+    :rtype: pandas.DataFrame
     """
 
-    print('Pulling ' + str(statement) + ' data for ' + str(df['symbol'].nunique()) + ' companies...')
+    print('Pulling ' + str(statement) + ' data for ' + str(df['symbol'].nunique())
+          + ' companies...')
 
     financial_statement = pd.DataFrame()
 
     for ticker in df['symbol']:
-        response = urlopen("https://financialmodelingprep.com/api/v3/financials/" + statement + "/" + ticker
-                           + "?period=" + period)
+        response = urlopen("https://financialmodelingprep.com/api/v3/financials/" + statement
+                           + "/" + ticker + "?period=" + period)
         data = response.read().decode("utf-8")
 
         try:
             data_json = json.loads(data)['financials']
             flattened_data = pd.json_normalize(data_json)
-            flattened_data.insert(0, 'symbol', ticker)
+            flattened_data.insert(0, 'symbol', ticker) # Change append to concat
             financial_statement = financial_statement.append(flattened_data, ignore_index=True)
 
         except KeyError:
             continue
 
-    print('Found ' + str(statement) + ' data for ' + str(financial_statement['symbol'].nunique()) + ' companies!')
+    print('Found ' + str(statement) + ' data for ' + str(financial_statement['symbol'].nunique())
+          + ' companies!')
 
     return financial_statement
 
 
 def clean_financial_statement(df):
     """
-    :param df: a df containing financial statement data (income, balance, or cash flow statement)
-    :return: a df with clean financial statement data (corrupted "date" rows removed, year column added)
+    Remove rows with corrupted date values, create new year column
+
+    :param df: DataFrame containing financial statement data
+    :return: Subset of the original DataFrame, with the addition of a new 'year' column
+    :rtype: pandas.DataFrame
     """
     print('Cleaning financial statement data for ' + str(df['symbol'].nunique()) + ' companies...')
 
@@ -81,23 +56,27 @@ def clean_financial_statement(df):
 
     clean_data['year'] = clean_data['date'].str[:4]
 
-    print('Returning clean financial statement data for ' + str(clean_data['symbol'].nunique()) + ' companies!')
+    print('Returning clean financial statement data for ' + str(clean_data['symbol'].nunique())
+          + ' companies!')
 
     return clean_data
 
 
-def subset_financial_statement(df, statement_year, eval_period):
+def select_statement_years(df, statement_year, eval_period):
     """
-    :param df: takes a df with a year column
-    :param statement_year: represents most recent financial statement year (e.g., 2019) required
-    :param eval_period: represents number of years leading up to the statement year that we'd like to analyze
-    :return: a df that is a subset of the original df provided
+    Remove companies without recent financial statements, subset data to evaluation period provided.
+
+    :param df: DataFrame containing financial statement data
+    :param statement_year: year of most recent financial statement
+    :param eval_period: number of years prior to most recent statement to be analyzed
+    :return: Subset of the DataFrame provided
+    :rtype: pandas.DataFrame
     """
 
-    print('Pulling data from ' + str(statement_year - eval_period) + ' to ' + str(statement_year) + ' for '
-          + str(df['symbol'].nunique()) + ' companies...')
+    print('Pulling data from ' + str(statement_year - eval_period) + ' to ' + str(statement_year)
+          + ' for ' + str(df['symbol'].nunique()) + ' companies...')
 
-    # Remove tickers without recent financial reports
+    # Remove tickers without recent financial reports (move to clean function)
     latest_statement = df.groupby(['symbol'])['year'].max()
     latest_statement_filter = latest_statement[latest_statement == str(statement_year)]
     recent_reporters = df[df['symbol'].isin(latest_statement_filter.index)]
@@ -106,55 +85,20 @@ def subset_financial_statement(df, statement_year, eval_period):
     year_filter = int(statement_year) - int(eval_period)
     recent_data = recent_reporters[(recent_reporters['year'].astype(int) >= year_filter)]
 
-    print('Found data from ' + str(statement_year - eval_period) + ' to ' + str(statement_year) + ' for '
-          + str(df['symbol'].nunique()) + ' companies!')
+    print('Found data from ' + str(statement_year - eval_period) + ' to ' + str(statement_year)
+          + ' for ' + str(df['symbol'].nunique()) + ' companies!')
 
     return recent_data
 
 
-# get all three financial statements joined together after cleaning
-    # Necessary to calculate all of the category metrics listed below
-    # Some metrics require numbers from more than one statement
 def join_financial_statements(income_statement, balance_sheet, cash_flow_statement):
     pass
 
-# research annual vs. quarterly reporting
+
+def main():
+    company_profiles = pd.read_csv('data/company_profiles.csv')
+    print(company_profiles.head())
 
 
-# profitability
-def calculate_profitability_metrics(df):
-    pass
-# Takes in the joined financial statements df
-# Returns a separate profitability metrics df
-
-# liquidity
-
-# solvency
-
-# cash flow
-
-# activity
-
-
-# FOR EACH COLUMN SELECTED: pivot and calculate ttm and 1, 3, 5 year growth rates and then unpivot / transpose
-def calculate_metric_growth(df):
-    pass
-# Takes in profitability metrics (or other) df
-# Returns profitability metrics df with new growth columns
-
-
-def join_metrics(profitability, liquidity, solvency, cashflow, activity):
-    pass
-
-
-def filter_companies_on_metrics(df):
-    pass
-
-
-companies = pd.read_csv('company_profiles.csv')
-sect = select_sector(companies, 'Consumer Defensive')
-ind = select_industry(sect, 'Consumer Packaged Goods')
-income_statement_annual = get_financial_statement(ind, 'income-statement', 'annual')
-income_statement_clean = clean_financial_statement(income_statement_annual)
-income_statement_subset = subset_financial_statement(income_statement_clean, 2019, 5)
-print(income_statement_subset)
+if __name__ == '__main__':
+    main()
