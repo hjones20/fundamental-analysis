0f23288b0402e96503d809e1411f0e6d46839c1b
hjones20 <chulahoma1@gmail.com>
2020-05-10 23:58:49 -0400

    Draft core functionality for fundamental analysis

--- fundamental/company_fundamentals.py
+++ fundamental/company_fundamentals.py
@@ -3,176 +3,62 @@ import pandas as pd
 pd.options.display.max_columns = 20
 pd.options.display.max_rows = 100
 
+dat = pd.read_csv('data/company-key-metrics.csv')
 
-def profitability_metrics(df):
+
+def get_trailing_twelve_months(df):
     """
-    Calculate profitability metrics for N companies.
+    Subset DataFrame to trailing twelve months of data.
 
-    :param df: DataFrame containing financial statement data on N companies
-    :return: DataFrame containing profitability metrics for N companies
+    :param df: DataFrame containing stock tickers (symbol) and dates for N companies
+    :return: Subset of the DataFrame provided
     :rtype: pandas.DataFrame
     """
-    metrics = df[['symbol', 'date']].copy()
+    df.sort_values(by=['symbol', 'date'], inplace=True, ascending=False)
+    df = df.groupby('symbol').nth(0)
 
-    metrics['Gross Profit Margin'] = df['Gross Profit'] / df['Revenue']
-    metrics['EBITDA Margin'] = df['EBITDA'] / df['Revenue']
-    metrics['Operating Profit Margin'] = df['Operating Income'] / df['Revenue']
-    metrics['Net Profit Margin'] = df['Net Income'] / df['Revenue']
-
-    metrics['COGS to Sales'] = df['Cost of Revenue'] / df['Revenue']
-
-    metrics['SG&A to Sales'] = df['SG&A Expense'] / df['Revenue']
-    metrics['R&D to Sales'] = df['R&D Expenses'] / df['Revenue']
-    metrics['Operating Expense to Sales'] = df['Operating Expenses'] / df['Revenue']
-
-    metrics['Interest Expense to Sales'] = df['Interest Expense'] / df['Revenue']
-    metrics['Income Tax to Sales'] = df['Income Tax Expense'] / df['Revenue']
-
-    # metrics['Return on Equity'] = df['Net Income'] / df['Total assets'] - df['Total liabilities']
-    # metrics['Return on Assets'] = df['Net Income'] / df['Total assets']
-    # Return on Invested Capital (key metrics)
-    #
-    # metrics['Book Value'] = df['Total assets'] - df['Total liabilities']
-    # metrics['Owners Earnings or FCF'] = df['Operating Cash Flow'] - df['Capital Expenditure']
-    # metrics['FCF Margin'] = (df['Operating Cash Flow'] - df['Capital Expenditure']) /df['Revenue']
-    #
-    # metrics['Cash Flow to CAPEX Ratio'] = df['Operating Cash Flow'] / df['Capital Expenditure']
-
-    return metrics
+    return df
 
 
-def liquidity_metrics(df):
+def filter_metrics(df, threshold, *args):
     """
-    Calculate liquidity metrics for N companies.
+    Subset DataFrame to stocks with column values above the specified threshold.
 
-    :param df: DataFrame containing financial statement data on N companies
-    :return: DataFrame containing liquidity metrics for N companies
+    :param df: DataFrame containing the column names specified in *args
+    :param threshold: Number that each column value should exceed
+    :param args: Column values to subset
+    :return: Subset of the DataFrame provided
     :rtype: pandas.DataFrame
     """
-    metrics = df[['symbol', 'date']].copy()
+    for arg in args:
+        df = df[df[arg] > threshold]
 
-    metrics['Current Ratio'] = df['Total current assets'] / df['Total current liabilities']
-    metrics['Quick Ratio'] = (df['Cash and cash equivalents'] + df['Short-term investments']
-                              + df['Receivables']) / df['Total current liabilities']
-    metrics['Cash Ratio'] = df['Cash and cash equivalents'] / df['Total current liabilities']
-
-    # DSO = Days Sales Outstanding (key metrics)
-    # DIO = Days Inventory Outstanding (key metrics - "Days of Inventory on Hand")
-    # DPO = Days Payables Outstanding (key metrics)
-
-    # Operating Cycle = DSO + DIO (calculate from metrics above)
-    # Cash Conversion Cycle = DSO + DIO - DPO (calculate from metrics above)
-
-    return metrics
+    return df
 
 
-def solvency_metrics(df):
-    """
-    Calculate solvency metrics for N companies.
+def get_growth_rates(df, report_year, eval_period, *args):
 
-    :param df: DataFrame containing financial statement data on N companies
-    :return: DataFrame containing solvency metrics for N companies
-    :rtype: pandas.DataFrame
-    """
-    metrics = df[['symbol', 'date']].copy()
+    df.sort_values(by=['symbol', 'date'], inplace=True, ascending=False)
+    df['year'] = df['date'].str[:4]
 
-    metrics['Debt Ratio'] = df['Total liabilities'] / df['Total assets']
-    metrics['Debt to Equity Ratio'] = df['Total Debt'] / df['Total shareholders equity']
-    metrics['Interest Coverage Ratio'] = df['EBIT'] / df['Interest Expense']
-    metrics['Cash Flow to Debt Ratio'] = df['Operating Cash Flow'] / df['Total Debt']
-    metrics['Working Capital to Debt Ratio'] = \
-        (df['Total current assets'] - df['Total current liabilities']) / (df['Short-term debt'] +
-                                                                          df['Long-term debt'])
+    year_filter = report_year - eval_period
+    df = df[df['year'].astype(int) >= year_filter]
 
-
-def cashflow_metrics(df):
-    """
-    Calculate cash-flow metrics for N companies.
-
-    :param df: DataFrame containing financial statement data on N companies
-    :return: DataFrame containing cash-flow metrics for N companies
-    :rtype: pandas.DataFrame
-     """
-    metrics = df[['symbol', 'date', 'Operating Cash Flow']].copy()
-
-    metrics['Operating Cash Flow Margin'] = df['Operating Cash Flow'] / df['Revenue']
-    metrics['Free Cash Flow'] = df['Operating Cash Flow'] - df['Capital Expenditure']
-
-    return metrics
+    for arg in args:
+        metric = df[['symbol', 'year', arg]]
+        metric.index = metric['year']
+        print(metric.transpose())
+        # Create pivot table instead
+        # Year = cols
+        # Symbol = rows
+        # Metric = values
 
 
-def activity_metrics(df):
-    """
-    Calculate activity metrics for N companies.
-
-    :param df: DataFrame containing financial statement data on N companies
-    :return: DataFrame containing activity metrics for N companies
-    :rtype: pandas.DataFrame
-    """
-    # Cash to Current Assets
-    # Inventory to Current Assets
-    # Accounts Receivable to Current Assets
-    # Accounts Payable to Current Assets
-
-    # SG&A as % of gross profit (less than 30% is great - consistency indicates DCA)
-    # Depreciation as % of gross profit
-
-
-def join_metrics(profitability, liquidity, solvency, cashflow, activity):
-    """
-    Join metric data for all stock tickers provided.
-
-    :param profitability: DataFrame containing profitability data on N companies
-    :param liquidity: DataFrame containing liquidity data on N companies
-    :param solvency: DataFrame containing solvency data on N companies
-    :param cashflow: DataFrame containing cashflow data on N companies
-    :param activity: DataFrame containing activity data on N companies
-    :return: DataFrame with the five provided metric DataFrames inner joined
-    :rtype: pandas.DataFrame
-    """
+def get_growth_stability(df, *args):
     pass
 
 
-def metric_growth(df, period):
-    """
-    Calculate period growth rates of core metrics, write to csv.
-
-    :param df: DataFrame containing previously calculated profitability, liquidity, etc. metrics
-    :param period: String 'annual' or 'quarter'
-    :return: DataFrame provided, plus additional growth-rate columns
-    :rtype: pandas.DataFrame
-    """
-    pass
-    # subset to list of columns we need to calculate growth for...
-    # FOR EACH COLUMN SELECTED: pivot and calculate ttm and 1, 3, 5 year growth rates and then
-    # unpivot / transpose
-
-# ======================================
-# ADD COMMON SIZE ANALYSIS FUNCTION
-# ======================================
-
-
-def main():
-    company_financials = pd.read_csv('data/company_financials.csv')
-
-    company_profitability = profitability_metrics(company_financials)
-    # company_liquidity = liquidity_metrics(company_financials)
-    # company_solvency = solvency_metrics(company_financials)
-    # company_cashflow = cashflow_metrics(company_financials)
-    # company_activity = activity_metrics(company_financials)
-    #
-    # joined_metrics = join_metrics(company_profitability,
-    #                               company_liquidity,
-    #                               company_solvency,
-    #                               company_cashflow,
-    #                               company_activity)
-    #
-    # growth_metrics = metric_growth(joined_metrics, 'annual')
-    #
-    # return growth_metrics
-    print(company_financials.columns)
-    print(company_profitability.head(20))
-
-
-if __name__ == '__main__':
-    main()
+# For loop: read everything in as DataFrame with variable name matching filename
+ttm_dat = get_trailing_twelve_months(dat)
+filtered_dat = filter_metrics(ttm_dat, 0, 'ROE', 'Interest Coverage', 'Free Cash Flow per Share')
+print(get_growth_rates(dat, 2019, 5, 'ROE', 'Interest Coverage', 'Free Cash Flow per Share'))
