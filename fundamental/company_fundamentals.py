00cc2de4dab13bde1b27e31fdeb7e0333914bec0
hjones20 <chulahoma1@gmail.com>
2020-05-16 00:55:11 -0400

    Refactor function to accept mean, median, percent change via stat argument

--- fundamental/company_fundamentals.py
+++ fundamental/company_fundamentals.py
@@ -7,7 +7,7 @@ pd.options.display.max_rows = 100
 
 def prepare_data(directory):
     """
-    Load and join all files in the provided directory
+    Load all files in the provided directory as pandas DataFrames and join data as necessary.
 
     :return: Master DataFrame containing all data from the listed directory
     :rtype: pandas.DataFrame
@@ -37,20 +37,17 @@ def prepare_data(directory):
     return master
 
 
-def calculate_descriptive_stats(df, stat, report_year, eval_period, *args):
-    pass
-
-
-def calculate_growth_rates(df, report_year, eval_period, *args):
+def calculate_stats(df, stat, report_year, eval_period, *args):
     """
-    Calculate N year growth rates for provided metrics; tickers without report_year financials
-    are removed
+    Calculate N year statistics for provided columns. Tickers without financials as recent as
+    the specified report_year are removed.
 
     :param df: DataFrame containing the columns specified in *args
-    :param report_year: Ending year of growth rate calculation
-    :param eval_period: Number of years to include in growth rate calculation
-    :param args: Columns that require growth rate calculations
-    :return: New DataFrame containing 'symbol', 'year', and 'percent change' columns
+    :param stat: Statistic to calculate: mean, median, or percent change
+    :param report_year: Ending year of calculation
+    :param eval_period: Number of years to include in the calculation
+    :param args: Columns that require calculations
+    :return: New DataFrame containing 'symbol', 'year', and 'stat X' columns
     :rtype: pandas.DataFrame
     """
 
@@ -63,30 +60,35 @@ def calculate_growth_rates(df, report_year, eval_period, *args):
     year_filter = report_year - eval_period
     df = df[df['year'].astype(int) >= year_filter]
 
-    company_growth_rates = pd.DataFrame()
+    company_stats = pd.DataFrame()
 
     for arg in args:
 
         metric = df[['symbol', 'year', arg]]
         metric = metric.pivot_table(values=arg, index='symbol', columns='year')
 
-        growth_rate = (metric.iloc[:, -1] / metric.iloc[:, 0]) - 1
-        column_name = str(eval_period) + 'Y ' + arg + ' % Change'
+        if stat == 'percent change':
+            column_name = str(eval_period) + 'Y ' + arg + ' % Change'
+            company_stats[column_name] = (metric.iloc[:, -1] / metric.iloc[:, 0]) - 1
 
-        company_growth_rates[column_name] = growth_rate
+        elif stat == 'mean':
+            column_name = str(eval_period) + 'Y ' + arg + ' Mean'
+            company_stats[column_name] = metric.mean(axis=1)
 
-    return company_growth_rates
+        elif stat == 'median':
+            column_name = str(eval_period) + 'Y ' + arg + ' Median'
+            company_stats[column_name] = metric.median(axis=1)
 
+    company_stats['year'] = report_year
 
-def plot_growth_stability(df, *args):
-    pass
+    return company_stats
 
 
 # Change this to **kwargs: metric, value threshold
 # Need min and max thresholds
-def screen_metrics(df, threshold, *args):
+def screen_stocks(df, threshold, *args):
     """
-    Subset DataFrame to stocks with column values above the specified threshold.
+    Subset DataFrame to stocks containing column values within the specified thresholds.
 
     :param df: DataFrame containing the columns specified in *args
     :param threshold: Number that each column value should exceed
@@ -100,38 +102,18 @@ def screen_metrics(df, threshold, *args):
     return df
 
 
-# def main():
-    # company_profiles = pd.read_csv('data/company-profiles.csv')
-    # company_financials = pd.read_csv('data/financials.csv')
-    # company_key_metrics = pd.read_csv('data/company-key-metrics.csv')
-    # company_financial_growth = pd.read_csv('data/financial-statement-growth.csv')
-    #
-    # profiles = company_profiles[['symbol', 'companyName', 'sector', 'industry', 'website']].copy()
-    #
-    # financials = company_financials[['symbol', 'year', 'Gross Margin', 'EBIT Margin',
-    #                                  'Net Profit Margin', 'Revenue', 'Net Income', 'EPS',
-    #                                  'Total assets', 'Total liabilities', 'Total debt',
-    #                                  'Free Cash Flow', 'Dividend payments']].copy()
-    #
-    # key_metrics = company_key_metrics[['symbol', 'year', 'Market Cap', 'PE ratio', 'PB ratio',
-    #                                    'Debt to Equity', 'Current ratio', 'Interest Coverage',
-    #                                    'ROE', 'ROIC', 'Book Value per Share',
-    #                                    'Free Cash Flow per Share', 'Shareholders Equity per Share',
-    #                                    'Days Sales Outstanding', 'Days of Inventory on Hand',
-    #                                    'Days Payables Outstanding']].copy()
-    #
-    # financial_growth = company_financial_growth[[]]
-    #
-    # merged = pd.merge(company_financials, company_key_metrics, on=['symbol', 'year'])
-    #
-    # growth_rates = calculate_growth_rates(merged, 2019, 10, 'Revenue', 'Net Income', 'EPS',
-    #                                       'Book Value per Share', 'Free Cash Flow per Share')
-    #
+def plot_performance(df, *args):
+    pass
+
+
+def main():
+    data = prepare_data('data/')
+
+    stats = calculate_stats(data, 'median', 2019, 2, 'Revenue', 'Net Income')
+    print(stats)
+
     # ttm = merged[merged.year == 2019]
     #
-    # ttm_eval = pd.merge(ttm, growth_rates, on='symbol')
-    # print(ttm_eval.head(10))
-
     # subset = df[(df['Debt to Equity'] < 0.5)
     #             & (df['Current ratio'] > 1.5)
     #             & (df['ROE'] > 0.10)
@@ -140,5 +122,5 @@ def screen_metrics(df, threshold, *args):
     # print(subset.shape)
 
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
